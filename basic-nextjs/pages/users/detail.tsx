dfb81b868001cc9564bca6179af278c594bf41ba
Bagus Nugroho <papaobobo@gmail.com>
2022-09-20 20:02:33 +0700

    basic routing

---
+++ basic-nextjs/pages/users/detail.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserDetail = () => {
+    return (
+        <div>
+            <p>User Detail Page</p>
+        </div>
+    );
+};
+
+export default UserDetail;
---
+++ basic-nextjs/pages/users/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            Users Page
+        </div>
+    );
+};
+
+export default index;
