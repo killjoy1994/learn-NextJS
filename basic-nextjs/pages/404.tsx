b0bf39f089010bb07405a1d3f3bd90263aa3fa72
Bagus Nugroho <papaobobo@gmail.com>
2022-09-21 09:41:01 +0700

    create 404 page

---
+++ basic-nextjs/pages/404.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const custom404 = () => {
+  return (
+    <div>
+      <h1 className="not-found-title">Ooooops...</h1>;
+      <h1 className="not-found-title">
+        Halaman yang anda cari tidak ditemukan...
+      </h1>
+      ;
+    </div>
+  );
+};
+
+export default custom404;
