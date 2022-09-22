3131875133d1989e9ebefd3276d0df227fd53138
Bagus Nugroho <papaobobo@gmail.com>
2022-09-22 22:46:30 +0700

    add auto redirect

--- basic-nextjs/pages/404.tsx
+++ basic-nextjs/pages/404.tsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const custom404 = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("dipanggil setelah 2 detik");
+      router.push("/");
+    }, 2000);
+  }, []);
+
   return (
     <div>
       <h1 className="not-found-title">Ooooops...</h1>;
