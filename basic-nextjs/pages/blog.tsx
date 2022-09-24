94994f5def22fcf638e3748f6210b36334e6d56e
Bagus Nugroho <papaobobo@gmail.com>
2022-09-25 01:06:01 +0700

    use getServerSideProps

--- basic-nextjs/pages/blog.tsx
+++ basic-nextjs/pages/blog.tsx
@@ -1,12 +1,41 @@
 import Link from "next/link";
 import Layout from "../Components/Layout";
+import styles from "../styles/Blog.module.css";
 
-const Blog = () => {
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface BlogProps {
+  dataBlog: Post[];
+}
+
+const Blog = (props: BlogProps) => {
+  const { dataBlog } = props;
   return (
     <Layout pageTitle="Blog Page">
-      <p>Blog Page</p>
+      {dataBlog.map((blog) => {
+        return (
+          <div key={blog.id} className={styles.card}>
+            <p className={styles["blog-title"]}>{blog.title}</p>
+            <p>{blog.body}</p>
+          </div>
+        );
+      })}
     </Layout>
   );
 };
 
 export default Blog;
+
+export async function getServerSideProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const dataBlog = await response.json();
+  return {
+    props: {
+      dataBlog,
+    },
+  };
+}
