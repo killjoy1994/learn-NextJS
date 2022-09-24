52a70c2956c192f7a5bb5ff343a1e6bff573ddfd
Bagus Nugroho <papaobobo@gmail.com>
2022-09-25 00:48:07 +0700

    use getStaticPaths

--- basic-nextjs/pages/users/[id].tsx
+++ basic-nextjs/pages/users/[id].tsx
@@ -2,16 +2,65 @@ import React from "react";
 import { useRouter } from "next/router";
 import Layout from "../../Components/Layout";
 
-const UserDetail = () => {
+interface UserDetailProps {
+  user: User;
+}
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+}
+
+const UserDetail = (props: UserDetailProps) => {
+  const { user } = props;
   const router = useRouter();
-  const { id } = router.query;
+
   return (
-    <Layout pageTitle="user-detaul">
+    <Layout pageTitle="user-detail">
       <div>
-        <p>User Detail Page {id}</p>
+        <p>{user.name}</p>
+        <p>{user.email}</p>
+        <p>{user.phone}</p>
+        <p>{user.website}</p>
       </div>
     </Layout>
   );
 };
 
 export default UserDetail;
+
+export async function getStaticPaths() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const dataUsers = await response.json();
+  const paths = dataUsers.map((user: User) => ({
+    params: {
+      id: user.id + "",
+    },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+interface GetStaticProps {
+  params: {
+    id: string;
+  };
+}
+
+export async function getStaticProps(context: GetStaticProps) {
+  const { id } = context.params;
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/users/" + id
+  );
+  const user = await response.json();
+  return {
+    props: {
+      user,
+    },
+  };
+}
--- basic-nextjs/pages/users/index.tsx
+++ basic-nextjs/pages/users/index.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import Layout from "../../Components/Layout";
+import styles from "./Users.module.css";
 
 interface UserProps {
   dataUsers: Array<any>;
 }
 
 export default function Users(props: UserProps) {
+  const router = useRouter();
+
   const { dataUsers } = props;
   console.log(dataUsers);
   return (
     <Layout pageTitle="Users Page">
       {dataUsers.map((user, idx) => {
         return (
-          <div key={idx.toString()}>
+          <div
+            className={styles.card}
+            key={user.id}
+            onClick={() => router.push(`/users/${user.id}`)}
+          >
             <p>{user.name}</p>
             <p>{user.email}</p>
           </div>
